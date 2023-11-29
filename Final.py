8ee59978fce734e7cff00020f76d0d08ad291b18
kakabest <144709984+kakabest@users.noreply.github.com>
2023-11-28 19:51:40 -0600

    upload a py file

--- Final.py
+++ Final.py
@@ -2,15 +2,20 @@ import streamlit as st
 from streamlit_extras.mandatory_date_range import date_range_picker 
 
 def example():
-    st.write(
-        """
-        This is an example of a date range picker that *always* returns a start and
-        end date, even if the user has only selected one of the dates. Until the
-        user selects both dates, the app will not run.
-        """
-    )
     result = date_range_picker("Select a date range")
     st.write("Result:", result)
 
 example()
 
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see #*
+st.caption('Balloons. Hundreds of them...')
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
+
+# * optional kwarg unsafe_allow_html = True
