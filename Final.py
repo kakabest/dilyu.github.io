f0e847d8e1b77096a0323e7651ebc09d0e6f4fd7
kakabest <144709984+kakabest@users.noreply.github.com>
2023-11-28 19:07:42 -0600

    upload a py file

--- Final.py
+++ Final.py
@@ -1,17 +1,9 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import math
-raw_data['hour'] = pd.DatetimeIndex(raw_data['CRASH TIME']).hour
-num_by_year = raw_data.groupby('hour')['CRASH DATE'].count()
+import pandas as pd
 
-plt.figure(figsize=(15, 7))
-num_by_year.plot(kind='line', marker='o', color='blue')
-plt.title('Number of Collections by Hour')
-plt.xlabel('CRASH HOUR')
-plt.ylabel('Number of CRASH')
-plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-plt.tight_layout()
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
 
-plt.show()
+st.dataframe(dataframe.style.highlight_max(axis=0))
