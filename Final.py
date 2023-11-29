6fc2c2bb70a981198b5097e8aa880728094d9a5d
kakabest <144709984+kakabest@users.noreply.github.com>
2023-11-28 20:10:06 -0600

    upload a py file

--- Final.py
+++ Final.py
@@ -3,7 +3,7 @@ from streamlit_extras.mandatory_date_range import date_range_picker
 
 def example():
     result = date_range_picker("Select a date range")
-    st.title('Streamlit Test')
+    st.title('Stream Test')
     st.write('Hello, Streamlit!')
 
 example()
