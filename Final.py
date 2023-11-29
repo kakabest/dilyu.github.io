5ea3061a5d5735b6a28ed6c38d315260fb0e029f
kakabest <144709984+kakabest@users.noreply.github.com>
2023-11-28 19:58:43 -0600

    upload a py file

--- Final.py
+++ Final.py
@@ -3,20 +3,8 @@ from streamlit_extras.mandatory_date_range import date_range_picker
 
 def example():
     result = date_range_picker("Select a date range")
-    st.write("Result:", result)
-    st.text('Fixed width text')
-    st.markdown('_Markdown_') # see #*
-    st.caption('Balloons. Hundreds of them...')
-    st.latex(r''' e^{i\pi} + 1 = 0 ''')
-    st.write('Most objects') # df, err, func, keras!
-    st.write(['st', 'is <', 3]) # see *
-    st.title('My title')
-    st.header('My header')
-    st.subheader('My sub')
-    st.code('for i in range(8): foo()')
+    st.title('Streamlit Test')
+    st.write('Hello, Streamlit!')
 
 example()
 
-
-
-# * optional kwarg unsafe_allow_html = True
